2542a342d9c47e7ef0d8a1bd7e0fde0c40894233
Vidar Rehn <vidar.rehn@gmail.com>
2022-06-01 18:55:35 +0200

    created post form in frontend

--- static/main.js
+++ static/main.js
@@ -1,9 +1,28 @@
 
 const getAllProducts = async () => {
-    const response = await fetch('https://vidars-shopping-list-api.herokuapp.com/products')
+    const response = await fetch('/products')
     const data = await response.json()
     return data
 }
 
 
 getAllProducts().then(data => console.log(data))
+
+const addProductForm = document.querySelector('#new-product')
+const productName = document.querySelector('#name')
+const productCategory = document.querySelector('#category')
+
+addProductForm.addEventListener('submit', async (e) => {
+    e.preventDefault()
+
+    await fetch('/products', {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name: productName.value,
+            category: productCategory.value
+        })
+    })
+})
